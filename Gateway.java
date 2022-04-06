562d71732f201835cc74e692a9dbbb0842f18c8f
shamalweerasooriya <e17380@eng.pdn.ac.lk>
2022-04-06 06:14:00 +0530

    create tcp connection

--- Gateway.java
+++ Gateway.java
@@ -1,8 +1,16 @@
 import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.Socket;
 import java.net.SocketException;
+
+import javax.xml.transform.OutputKeys;
 
 // This is the gateway server to listen for monitor requests and create a tcp connection to the monitor.
 public class Gateway {
@@ -26,9 +34,20 @@ public class Gateway {
             DatagramPacket recvPacket = recvPacket(recvSocket);
 
             // print ip address and port of the monitor
-            printIPAndPort(recvPacket);
+            // printIPAndPort(recvPacket);
 
-            // create a tcp connection to the monitor
+            // create tcp thread to connect to the monitor
+            Thread tcpConnection = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    // create a tcp connection to the monitor
+                    createTcpConnection(recvPacket);
+                }
+            });
+
+            // start the tcp connection thread
+            tcpConnection.start();
+
         }
 
         // try {
@@ -89,6 +108,57 @@ public class Gateway {
 
             // print ip address and port number
             System.out.println("IP: " + monitor.getIp() + ", Port: " + monitor.getPort());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // create a tcp connection to the monitor
+    private static void createTcpConnection(DatagramPacket recvPacket) {
+        try {
+            // byte array input stream
+            ByteArrayInputStream bis = new ByteArrayInputStream(recvPacket.getData());
+            ObjectInputStream ois = new ObjectInputStream(bis);
+
+            // get monitor object from the received packet
+            Monitor monitor = (Monitor) ois.readObject();
+
+            // save ip address, port number and id of the monitor
+            InetAddress ip = monitor.getIp();
+            int port = monitor.getPort();
+            String id = monitor.getMonitorID();
+
+            // create a client socket to connect to the monitor
+            Socket clientSocket = new Socket(ip, port);
+
+            // print monitor info
+            // System.out.println(monitor.monitor_str());
+
+            // decalre streams to send and receive data
+            OutputStream os = clientSocket.getOutputStream();
+            OutputStreamWriter osw = new OutputStreamWriter(os);
+            osw.write("Requesting data from " + id + "\n");
+            osw.flush();    // request sent
+
+            // declaring input stream
+            InputStream is = clientSocket.getInputStream();
+            InputStreamReader isr = new InputStreamReader(is);
+
+            // buffer to store data received
+            StringBuffer buffer = new StringBuffer();
+
+            // read data from the monitor
+            int data = isr.read();
+            while (data != '\n') {
+                buffer.append((char) data);
+                data = isr.read();
+            }
+
+            System.out.println(buffer.toString());
+
+            // close the socket
+            clientSocket.close();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
